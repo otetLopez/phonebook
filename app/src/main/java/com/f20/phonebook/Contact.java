da170ffeb8376d8659230c7b20edcb0e81fc4c15
otetLopez <otet.tud@gmail.com>
2020-02-18 16:57:07 -0500

    Added Contact Class

---
+++ app/src/main/java/com/f20/phonebook/Contact.java
@@ -0,0 +1,56 @@
+package com.f20.phonebook;
+
+public class Contact {
+    private int id;
+    private String fname;
+    private String lname;
+    private int phone;
+    private String address;
+
+    public Contact(String fname, String lname, int phone, String address) {
+        this.fname = fname;
+        this.lname = lname;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
--- app/src/main/java/com/f20/phonebook/MainActivity.java
+++ app/src/main/java/com/f20/phonebook/MainActivity.java
@@ -1,6 +1,7 @@
 package com.f20.phonebook;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
